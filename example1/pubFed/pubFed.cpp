97c5b21da7feac4ed3de60ac6dbe3910f3c83a03
Philip Top <phlptp@gmail.com>
2023-02-20 16:23:27 -0800

    Update example 1 to use HELICS3 (#6)

--- example1/pubFed/pubFed.cpp
+++ example1/pubFed/pubFed.cpp
@@ -1,16 +1,16 @@
-#include <ValueFederate.h>
+#include "helics/helics.h"
 #include <stdio.h> 
 
 int main()
 {
-	helics_federate_info infoStruct;
+	HelicsFederateInfo infoStruct;
 	const char* fedinitstring = "--federates=1"; // tells the broker to expect 1 federate
-	helics_federate controlFed; // creates an instance of a federate
-	helics_publication var; // creates an instance of a publication that would be used by the federate to pass along a message
-	helics_input received; // creates an instance of a subscription that would be used by the federate
-	helics_time currenttime = 0.0; //initializes the time for the federate to start 
-	helics_bool isUpdated;
-	helics_error err = helicsErrorInitialize(); // initializes an error variable to ensure that the simulation is working
+	HelicsFederate controlFed; // creates an instance of a federate
+	HelicsPublication var; // creates an instance of a publication that would be used by the federate to pass along a message
+	HelicsInput received; // creates an instance of a subscription that would be used by the federate
+	HelicsTime currenttime = 0.0; //initializes the time for the federate to start 
+	HelicsBool isUpdated;
+	HelicsError err = helicsErrorInitialize(); // initializes an error variable to ensure that the simulation is working
 
 	infoStruct = helicsCreateFederateInfo(); // creates a structure to hold information about the federate
 
@@ -19,20 +19,20 @@ int main()
 
 	helicsFederateInfoSetTimeProperty(infoStruct, helicsGetPropertyIndex("period"), 2.0, &err);
 
-	controlFed = helicsCreateValueFederate("pubFed.exe", infoStruct, &err);
+	controlFed = helicsCreateValueFederate("pubFed", infoStruct, &err);
 	
 	helicsFederateInfoFree(infoStruct);
-	if (err.error_code != helics_ok)
+	if (err.error_code != HELICS_OK)
 	{
 		return(-2);
 	}
 
-	var = helicsFederateRegisterGlobalPublication(controlFed, "time", helics_data_type_string, "", &err);
+	var = helicsFederateRegisterGlobalPublication(controlFed, "time", HELICS_DATA_TYPE_STRING, "", &err);
 	received = helicsFederateRegisterSubscription(controlFed, "sendback", NULL, &err); 
 
 	helicsFederateEnterInitializingMode(controlFed, &err);
 	
-	if (err.error_code != helics_ok)
+	if (err.error_code != HELICS_OK)
 	{
 		fprintf(stderr, "HELICS failed to enter initialization mode:%s\n", err.message);
 	}
@@ -43,7 +43,7 @@ int main()
 
 	helicsFederateEnterExecutingMode(controlFed, &err);
 	
-	if (err.error_code != helics_ok)
+	if (err.error_code != HELICS_OK)
 	{
 		fprintf(stderr, "HELICS failed to enter execution mode:%s\n", err.message);
 	}
@@ -55,7 +55,7 @@ int main()
 	helicsPublicationPublishString(var, "Hello!!", &err);
 
 	currenttime = helicsFederateRequestTime(controlFed, 2.0, &err);
-	if (err.error_code != helics_ok)
+	if (err.error_code != HELICS_OK)
 	{
 		fprintf(stderr, "HELICS request time failed:%s\n", err.message);
 	}
